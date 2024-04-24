faa48b12be4219e98cffe3738f67cda1ad2e5770
daltonpfdev <daltonpf.dev@gmail.com>
2024-04-24 11:43:41 -0300

    Front-end do projeto

--- galeria/views.py
+++ galeria/views.py
@@ -2,5 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("<h1>Hello World</h1><p>Bem vindo ao Espaço</p>")
+    return render(request, 'galeria/index.html')
 
+def imagem(request):
+    return render(request, 'galeria/imagem.html')
